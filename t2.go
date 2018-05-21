b1ee7f58887d526c2aed445a54589e47a96342fb
fy <420585251@qq.com>
2018-05-21 18:30:53 +0800

    update files

--- t2.go
+++ t2.go
@@ -30,30 +30,41 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"strconv"
 	"strings"
 	"sync"
-	//"time"
 )
 
 var waitGroup sync.WaitGroup
-var clients = make(map[string]net.Conn)
-var devices = make(map[string]net.Conn)
+var clientConn = make(map[string]net.Conn)
+var deviceConn = make(map[string]net.Conn)
+
 var queues = make(map[string]string)
 
-//var clients1 [][]string
+//var deviceList = make(map[int]string)
+var deviceList []string
+
+var groupList = make(map[string]interface{})
+
+/*var deviceList = make(map[int]string)
+ */
+
+var numbers []int
+
+//var clientConn1 [][]string
 
 //var queues map[string]string
 
 const (
-	tcpIp = "127.0.0.1:1000"
+	TCPIP = "0.0.0.0:1000"
 )
 
 func main() {
 	// 绑定地址
-	tcpAddr, err := net.ResolveTCPAddr("tcp", tcpIp)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", TCPIP)
 	if err != nil {
 		fmt.Println("绑定地址失败:", err.Error())
 		return
@@ -64,7 +75,7 @@ func main() {
 		fmt.Println("监听端口失败:", err.Error())
 		return
 	}
-	log.Println("已初始化连接,等待终端连接...")
+	log.Println("服务端:已初始化连接,等待终端连接...")
 	start(listen)
 }
 func start(listen *net.TCPListener) {
@@ -78,94 +89,162 @@ func start(listen *net.TCPListener) {
 		//fmt.Println(strings.Contains("widuu", "wi")) //true
 		//fmt.Println(strings.Contains("wi", "widuu")) //false
 		//
-		var clientIp string = conn.RemoteAddr().String()
-		ipStrSplit := strings.Split(clientIp, ":")
-		port, err := strconv.Atoi(ipStrSplit[1])
-		//
-		if port < 500 {
-			devices[clientIp] = conn
-			log.Println("设备端:", clientIp+"上线")
-			log.Println(devices)
-		} else {
-			clients[clientIp] = conn
-			log.Println("客户端:", clientIp+"上线")
-			log.Println(clients)
-			conn.Write([]byte("deviceList"))
-		}
-		//
 		waitGroup.Add(1)
-		go handle(conn)
+		go handleTerminal(conn)
 	}
 	waitGroup.Wait()
 }
-func handle(conn net.Conn) {
+
+func handleTerminal(conn net.Conn) {
 	//conn.SetReadDeadline(time.Now().Add(2 * time.Minute))
 	defer conn.Close()
+	//
+	var remoteType = ""
+	var remoteIp = conn.RemoteAddr().String()
+	ipStrSplit := strings.Split(remoteIp, ":")
+	port, _ := strconv.Atoi(ipStrSplit[1])
+	// 据端口区分
+	if port < 500 {
+		remoteType = "device"
+	} else {
+		remoteType = "client"
+	}
+	// 终端类型
+	if remoteType == "device" {
+		deviceConn[remoteIp] = conn
+		//deviceList[port] = terminalIp
+		deviceList = append(deviceList, remoteIp)
+		log.Println("设备端:" + remoteIp + "上线")
+		log.Println("设备列:", deviceConn)
+	} else {
+		clientConn[remoteIp] = conn
+		log.Println("客户端:" + remoteIp + "上线")
+		log.Println("客户列:", clientConn)
+		log.Println("设备列:", deviceList)
+		var deviceStr = ""
+		for i := 0; i < len(deviceList); i++ {
+			// 以byte方式按字节遍历
+			if deviceStr == "" {
+				deviceStr = deviceList[i]
+			} else {
+				deviceStr = deviceStr + "," + deviceList[i]
+			}
+		}
+		//log.Println(deviceStr)
+		conn.Write([]byte("deviceList:" + deviceStr))
+	}
+	//
 	data := make([]byte, 1024)
 	for {
+		//
 		i, err := conn.Read(data)
-		if err != nil {
-			//fmt.Println("读取客户端数据错误:", err.Error())
-			//fmt.Println("conn closed")
-			DeleteClient(conn)
+		// 数据读取
+		if err != nil && err != io.EOF {
+			deleteConn(conn, remoteType, remoteIp)
 			break
 		}
+		// 退出指令
 		if string(data[:i]) == "exit" {
-			//conn.Write([]byte{'e', 'x', 'i', 't'})
+			deleteConn(conn, remoteType, remoteIp)
+			break
+		}
+		// json方式
+		/*jsonStr := string(data[0:i])
+		log.Println("终端:"+conn.RemoteAddr().String()+"发来数据:", jsonStr)
+		// 过滤标记
+		jsonStr = strings.Replace(jsonStr, "'", "\"", -1)
+		jsonStr = strings.Replace(jsonStr, "\n", "", -1)
+		// var dat map[string]interface{}
+		var dat map[string]interface{}
+		if err := json.Unmarshal([]byte(jsonStr), &dat); err == nil {
+			log.Println(dat)
+		} else {
+			log.Println(err)
+		}
+		// 设备序号
+		//var deviceId = dat["deviceId"]
+		//fmt.Println(deviceId, reflect.TypeOf(deviceId).String())
+		// 输入参数1
+		var deviceIp interface{}
+		var deviceCmd string
+		if _, ok := dat["deviceIp"]; ok {
+			deviceIp = dat["deviceIp"]
+		} else {
 			DeleteClient(conn)
 			break
 		}
-		log.Println("客户端:", conn.RemoteAddr().String()+"发来数据:", string(data[0:i]))
-		/*
-			var deviceNo = string(data[0:1])
-			if devices[deviceNo] {
-				fmt.Println(devices[deviceNo])
-			} else {
-				DeleteDevice(conn)
-			}
-		*/
-		var no = string(data[0:1])
-		var ip = ""
-		if no == "1" {
-			ip = "127.0.0.1:102"
-		} else if no == "2" {
-			ip = "127.0.0.1:101"
+		// 输入参数2
+		if _, ok2 := dat["action"]; ok2 {
+			//deviceCmd = dat["action"]
+			deviceCmd = "123"
 		} else {
-			DeleteDevice(conn)
+			DeleteClient(conn)
 			break
-		}
-		//
-		var share = devices[ip]
-		//fmt.Println(share)
-		//
-		if share != nil {
-			log.Println("服务端:", conn.RemoteAddr().String()+"=>"+share.RemoteAddr().String()+"数据:"+string(data[0:i]))
-			share.Write(data[2:i])
-			queues[no] = ip
-			fmt.Println(queues)
+		}*/
+
+		// 字符串方式
+		dataStr := string(data[0:i])
+		log.Println("终端:" + remoteIp + "=>" + dataStr)
+		// 过滤标记
+		// TODO 下标问题
+		dataSplit := strings.Split(dataStr, ",")
+		//fmt.Println(reflect.TypeOf(dataSplit), dataSplit)
+		// 验证输入参数
+		var deviceIp = dataSplit[0]
+		var deviceCmd = dataSplit[1]
+		// 当前设备是否已组队
+		groupIp := groupList[remoteIp]
+		if groupIp == deviceIp {
+			// 已组队
+			log.Println("服务端:检测到当前与设备端已存在组队" + deviceIp)
+			//
+			var relay = deviceConn[deviceIp]
+			//
+			if relay != nil {
+				log.Println("服务端:" + remoteIp + "=>" + relay.RemoteAddr().String() + "=>" + deviceCmd)
+				relay.Write([]byte(deviceCmd))
+			} else {
+				log.Println("设备端:" + deviceIp + "已下线")
+				conn.Write([]byte(deviceIp + ":exit"))
+			}
 		} else {
-			log.Println("设备端已下线")
-			conn.Write([]byte("exit"))
+			// 检测未组队情况下所选择的设备是否可用
+			var readOnly = false
+			for _, group := range groupList {
+				if deviceIp == group {
+					readOnly = true
+					break
+				}
+			}
+			// 进行组队
+			if !readOnly {
+				log.Println("服务端:" + remoteIp + "<=>" + deviceIp + "组队成功")
+				groupList[conn.RemoteAddr().String()] = deviceIp
+			} else {
+				log.Println("设备端:" + deviceIp + "已锁定")
+				conn.Write([]byte(deviceIp + ":locked"))
+				// TODO
+			}
 		}
+		//
+		log.Println(groupList)
 	}
 	// 任务完成
 	waitGroup.Done()
 }
-func DeleteClient(conn net.Conn) {
-	//fmt.Println(clients)
-	log.Println("客户端:", conn.RemoteAddr().String()+"下线")
-	delete(clients, conn.RemoteAddr().String())
-	log.Println(clients)
+
+// 删除连接
+func deleteConn(conn net.Conn, remoteType string, remoteIp string) {
+	if remoteType == "device" {
+		log.Println("设备端:" + remoteIp + "下线")
+		delete(deviceConn, remoteIp)
+		log.Println(deviceConn)
+	} else {
+		log.Println("客户端:" + remoteIp + "下线")
+		delete(clientConn, remoteIp)
+		log.Println(clientConn)
+	}
 	conn.Write([]byte("exit"))
-	return
-}
-func DeleteDevice(conn net.Conn) {
-	//fmt.Println(devices)
-	log.Println("设备端:", conn.RemoteAddr().String()+"下线")
-	delete(devices, conn.RemoteAddr().String())
-	log.Println(devices)
-	conn.Write([]byte("exit"))
-	return
 }
 
 /*
